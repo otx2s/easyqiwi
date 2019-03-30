c2993ff92fc8a288729963959d9695fcd569b4d9
mind0verflow69 <47149411+otx2s@users.noreply.github.com>
2019-03-30 10:05:08 +0700

    Add files via upload

--- core/vivod.py
+++ core/vivod.py
@@ -10,11 +10,16 @@ def viv():
 		pho2 = input(B+"[#]"+W+" Enter your phone: ")
 		amou = int(input(B+"[#]"+W+" Enter amount: "))
 		api  = QApi(token=tok, phone=pho)
-		if api.balance != 0:
-			print(G+"[+]"+W+" Balance Founded")
-			print(G+"[+]"+W+" Balance =", api.balance)
-			api.pay(account=pho2, amount=amou, comment="Sorry bro ;(")
-			print(G+"[+]"+W+" Balance =", api.balance)
+		if api.balance != 0 and pho != pho2:
+			try:
+				print(G+"[+]"+W+" Balance Founded")
+				print(G+"[+]"+W+" Balance =", api.balance)
+				api.pay(account=pho2, amount=amou, comment="Sorry bro ;(")
+				print(G+"[+]"+W+" Balance =", api.balance)
+			except:
+				print(R+"[X]"+W+" Balance found but unable to send money")
+		elif api.balance !=0 and pho == pho2:
+			print(R+"[X]"+W+" You can not transfer money from your account to yourself")
 	except:
-		print(R+"[X]"+W+" Unable to send money. Balance not found")
+		print(R+"[X]"+W+" Unable to send money")
 		pass
