89caad0a4d5c43b3dfe04528d7b3131362214679
mind0verflow69 <47149411+otx2s@users.noreply.github.com>
2019-03-30 10:13:38 +0700

    Add files via upload

--- core/main.py
+++ core/main.py
@@ -13,22 +13,25 @@ os.system("clear")
 
 def main():
 	print(banner)
-	van = input(">>> ")
-	if van == "1" or van == "01":
-		balance.bal()
-		main()
+	try:
+		van = input(">>> ")
+		if van == "1" or van == "01":
+			balance.bal()
+			main()
 
-	elif van == "2" or van == "02":
-		vivod.viv()
-		main()
+		elif van == "2" or van == "02":
+			vivod.viv()
+			main()
 
-	elif van == "3" or van == "03":
-		print(R+"\nBye, bye\n"+W)
+		elif van == "3" or van == "03":
+			print(R+"\nBye, bye\n"+W)
 
-	else:
-		print(R+"[X]"+W+" Wrong command -> " + van)
-		ts(1)
-		main()
+		else:
+			print(R+"[X]"+W+" Wrong command -> " + van)
+			ts(1)
+			main()
 
+	except(KeyboardInterrupt):
+		print(R+"[X]"+W+" Exiting...")
 if __name__ == '__main__':
 	main()
